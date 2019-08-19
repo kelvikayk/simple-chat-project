319cee830e181513e84812b6f1fad4ed949bc6b0
HenryLimaBR <djfilipebr@gmail.com>
2019-08-19 17:48:19 -0300

    Added "Enter Event" and New Styles

--- public/main.js
+++ public/main.js
@@ -1,1 +1,16 @@
+const textInput = document.getElementById("textInput");
+
 const socket = io.connect(":8080");
+
+var userdata = {
+    username: "Henry Lima",
+    userid: "henrylimabr",
+    message: ""
+}
+
+function keyEnter(e) {
+    if (e.keyCode === 13) {
+        userdata.message = textInput.value;
+        textInput.value = "";
+    }
+}
