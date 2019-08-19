434a6aa098014d1da5c28a6125b723805bf8897b
HenryLimaBR <djfilipebr@gmail.com>
2019-08-18 21:00:49 -0300

    Some Tweaks To Server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const server = express();
+const path = require("path");
 
 const port = process.env.port || 80;
 
+server.use("/", express.static("public"));
 server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 server.listen(port, () => {
     console.log(`WebServer Running at ${port} port!`);
